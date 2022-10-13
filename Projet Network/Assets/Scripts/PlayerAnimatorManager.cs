33342acda9d4fa8d79ec8515dbe563da53cbec9c
Ethan <e.dehay@rubika-edu.com>
2022-10-13 20:50:39 +0200

    Changements Character Controller

--- Projet Network/Assets/Scripts/PlayerAnimatorManager.cs
+++ Projet Network/Assets/Scripts/PlayerAnimatorManager.cs
@@ -28,6 +28,10 @@ public class PlayerAnimatorManager : MonoBehaviourPun
             return;
         }
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        
         AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
         if (stateInfo.IsName("Base Layer.Run"))
@@ -38,14 +42,51 @@ public class PlayerAnimatorManager : MonoBehaviourPun
             }
         }
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        if (v < 0)
+        if (stateInfo.IsName("Base Layer.Idle 0"))
         {
-            v = 0;
+            if (z == 0)
+            {
+                Vector3 rota = transform.rotation.eulerAngles;
+                rota = rota.y * new Vector3(0,z,0);
+                transform.rotation = Quaternion.Euler(rota);
+            }
         }
+        
+        
+        if(z >= 0)
+        {
+            transform.Translate(new Vector3(x * 0.01f,0,z * 0.01f));
+        }
+            
+        if(stateInfo.IsName("Base Layer.Run O"))
+        {
+            Debug.Log(z);
+            transform.Translate(new Vector3(0,0,10));
+        }
+        
 
-        animator.SetFloat("Speed", v * v);
-        animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down * 5))
+        {
+            Debug.DrawRay(transform.position, Vector3.down * 5, Color.green);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.down, Color.red);
+        }
+      
+        animator.SetFloat("Speed", z);
+        animator.SetFloat("Direction", x, directionDampTime, Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        transform.Translate((new Vector3(0,10,0)));
     }
 }
